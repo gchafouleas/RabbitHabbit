8e627a77da5cce01baff6705a62d0aace16b51de
gchafouleas <genevieve.chafouleas@gmail.com>
2015-03-03 15:27:15 -0500

    Added scenes and buttons controlls for main menu

---
+++ RabbitHabbit/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+	public void OnStartGameClick() 
+	{
+		Application.LoadLevel("GamePlay"); 
+	}
+
+	public void OnControlsClick() 
+	{
+		Application.LoadLevel("Controls"); 
+	}
+}
