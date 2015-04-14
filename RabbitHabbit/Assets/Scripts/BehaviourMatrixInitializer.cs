987cb966c281bea61bd977c0316b51e21a5cd741
Matthew St.G <mattstger@gmail.com>
2015-04-14 12:18:10 -0400

    Balanced & additional initializer
    Created a more balanced start weights for wolf

--- RabbitHabbit/Assets/Scripts/BehaviourMatrixInitializer.cs
+++ RabbitHabbit/Assets/Scripts/BehaviourMatrixInitializer.cs
@@ -6,17 +6,37 @@ public class BehaviourMatrixInitializer : MonoBehaviour {
 			
 	public float[,] Initialize()
 	{
-		//Use this class to set the initial values to balance the wolves
-		
-		float[,] toReturn = new float[GlobalVars.wolfStateLength,GlobalVars.wolfEventLength]
+		//Use this class to set the initial values to balance the wolves, returns one at random
+		float[,] toReturn;
+
+		//if((float)Random.Range(-20,20)/10 )
+		//if ((float)Random.Range(0, 1) == 0)
+		if(true) //for testing purposes, test the balanced wolf
 		{
-			//EventRows  SeeRabbit,SmellRabbit,OneWolfNear,TwoWolvesNear,ThreeWolvesNear,InStateFor10Seconds, HearHowl, friendAttacks, 
-							{0,			0,			0,			0,				0,				0,				0,				0}, //ChaseRabbit
-							{0,			0,			0,			0,				0,				0,				0,				0}, //Howl
-							{0,			0,			0,			0,				0,				0,				0,				0}, //Stalk
-							{0,			0,			0,			0,				0,				0,				0,				0}, //Sniff
-							{0,			0,			0,			0,				0,				0,				0,				0}, //Wander
-		};
+			//balanced weights
+			toReturn = new float[GlobalVars.wolfStateLength, GlobalVars.wolfEventLength]
+			{
+			//EventRows  SeeRabbit,SmellRabbit,OneWolfNear,TwoWolvesNear,ThreeWolvesNear,InStateFor30Seconds, HearHowl, friendAttacks, 
+							{.8f,		0,		   .2f,		   .4f,			   .8f,				0,				.2f,		  .6f}, //ChaseRabbit
+							{.6f,		0,		  -.2f,		  -.4f,			  -.6f,				0,				0,				0}, //Howl
+							{.6f,		0,		   .2f,		  -.3f,		      -.6f,				0,				0,				0}, //Stalk
+							{-2,		1,			0,			0,				0,				0,				0,				0}, //Sniff
+							{-2,	   -2,			0,			0,				0,				3,				0,				0}, //Wander
+			};
+		}
+		else
+		{
+			//more randomized weights
+			toReturn = new float[GlobalVars.wolfStateLength, GlobalVars.wolfEventLength]
+			{
+			//EventRows  SeeRabbit,SmellRabbit,OneWolfNear,TwoWolvesNear,ThreeWolvesNear,InStateFor30Seconds, HearHowl, friendAttacks, 
+							{.4f,		0,		   .4f,		   .4f,			   .5f,				0,				.3f,		  .8f}, //ChaseRabbit
+							{.2f,		0,		  -.6f,		  -.4f,			  -.2f,				.4f,			0,				0}, //Howl
+							{.8f,		0,		   .2f,		  -.6f,		      -.6f,				.5f,			0,				0}, //Stalk
+							{-1,		.4f,			0,			0,				 0,				0,				0,				0}, //Sniff
+							{-2,	   -2,			0,			0,			   .2f,				3,				.2f,				0}, //Wander
+			};
+		}
 
 		return toReturn;
 	}
